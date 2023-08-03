76768589ea9eefe1b6a04ff18acdb8290a250a73
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2023-08-04 02:05:18 +0300

    Add files via upload

---
+++ root/home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index),
+    path('contacts', contacts),
+    path('about', about),
+    path('testimonial', testimonial),
+    path('our_team', our_team),
+    path('service', service),
+
+
+]
